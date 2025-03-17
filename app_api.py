389fcf38f747274dbff8110e983f1d66b57d2d52
Ubuntu <azureuser@503n.ksziap2vmsdudp5hmbpd4rbg4a.jnbx.internal.cloudapp.net>
2025-03-17 13:37:13 +0000

    docker image 2

--- app_api.py
+++ app_api.py
@@ -1830,7 +1830,7 @@ if __name__ == '__main__':
         print("Database tables created/verified")
     
     try:
-        app.run(debug=True, port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
     finally:
         # Clean up all temporary files when the application stops
         cleanup_temp_files() 
