bdc6529f944e63ad140992f59bb712f3ff3bb2f0
Mahmoud Chaer <mmc55@mail.aub.edu>
2025-04-22 18:16:38 +0300

    pushing automated deployment to dockerhub

--- app_api.py
+++ app_api.py
@@ -65,7 +65,7 @@ SERVICES = {
 
 # Setup Flask app
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'your_secret_key')
+app.SECRET-KEY = os.getenv('SECRET-KEY', 'your_SECRET-KEY')
 UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'medicai_temp')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
